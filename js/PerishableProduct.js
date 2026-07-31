57345f774cf22c4d3d001e3ea22addf8b78be2a5
HailuBen <b.hailu.pk@gmail.com>
2026-07-30 22:57:47 -0400

    add constructor to sublclass, import

---
+++ js/PerishableProduct.js
@@ -0,0 +1,10 @@
+// subclass for expiration dated products
+
+import Product from "./Product";    // import base class to use its elements
+
+export default class PerishableProduct extends Product {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+}
