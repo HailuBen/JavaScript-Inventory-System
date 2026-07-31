54087ab308d7d8e5493d8c89a202a7adf2300ccb
HailuBen <b.hailu.pk@gmail.com>
2026-07-30 22:54:35 -0400

    add constructor, getTotalValue(), toString()

---
+++ js/Product.js
@@ -0,0 +1,20 @@
+//Base general product class
+
+export default class Product {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }   // 3 main elements of a product included in constructor
+
+    // for calculating the value of a product
+    getTotalValue() {
+        return this.price + this.quantity;
+    }
+
+    // displays a string representation of a product
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+
+}
